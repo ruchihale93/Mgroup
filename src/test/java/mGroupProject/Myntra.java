a234978a0edd7089104865b6b2029e9d47869c75
Ruchi <ruchihale93@gmail.com>
2023-08-03 02:34:34 -0700

    [ruchihale93] add new testcase Myntra

---
+++ src/test/java/mGroupProject/Myntra.java
@@ -0,0 +1,20 @@
+package mGroupProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Myntra {
+  @Test
+  public void MyntraTest() throws InterruptedException {
+	  
+	  WebDriver driver=new ChromeDriver();
+	  Reporter.log("Opening Myntra", true);
+	  driver.get("https://www.myntra.com/");
+	  Assert.fail();
+	  Thread.sleep(7000);
+	  driver.close();
+  }
+}
