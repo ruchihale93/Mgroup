03981922b71093243003179b82aabf8ffd9dc63e
Ruchi <ruchihale93@gmail.com>
2023-08-03 02:48:42 -0700

    [ruchihale93] adding new meeshow testcase

---
+++ src/test/java/mGroupProject/Meeshow.java
@@ -0,0 +1,20 @@
+package mGroupProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Meeshow {
+  @Test
+  public void Ec_123_Meeshowtest() throws InterruptedException {
+	  
+	  WebDriver driver=new ChromeDriver();
+	  Reporter.log("Opening Meesho", true);
+	  driver.get("https://www.meesho.com/");
+	  Assert.fail();
+	  Thread.sleep(7000);
+	  driver.close();
+  }
+}
