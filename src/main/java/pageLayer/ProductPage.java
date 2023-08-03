603a0f2a7313c04fcdceff9f581f7c4a5ba510ba
Ruchi <ruchihale93@gmail.com>
2023-08-03 00:28:54 -0700

    [ruchihale93] commiting 1st code of day

--- src/main/java/pageLayer/ProductPage.java
+++ src/main/java/pageLayer/ProductPage.java
@@ -1,5 +1,33 @@
 package pageLayer;
 
-public class ProductPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class ProductPage {
+	
+	public ProductPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	// object methods
+	
+	@FindBy(xpath="")
+	private WebElement add_to_cart_btn;
+	
+	@FindBy(xpath="")
+	private WebElement click_on_popup;
+	
+	//action methods
+	
+	public void clickOnCartBtn()
+	{
+		add_to_cart_btn.click();
+	}
+	
+	
+	
+	
 }
