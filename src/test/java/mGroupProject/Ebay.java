7a48783665e486ffb31b1cf6b8cfc71228b8ba39
Ruchi <ruchihale93@gmail.com>
2023-08-03 23:05:49 -0700

    [ruchihale93] making changes in Testcase

--- src/test/java/mGroupProject/Ebay.java
+++ src/test/java/mGroupProject/Ebay.java
@@ -1,9 +1,19 @@
 package mGroupProject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Ebay {
   @Test
-  public void f() {
+  public void Ebaytest() throws InterruptedException {
+	  
+	  WebDriver driver=new ChromeDriver();
+	  Reporter.log("Opening Ebay", true);
+	  driver.get("https://www.ebay.com/");
+	
+	  Thread.sleep(7000);
+	  driver.close();
   }
 }
